0514269a85f3b37a5a88c9f1a7e35a779cd79eee
jkozub <j.kozub136@gmail.com>
2023-10-14 14:49:00 +0200

    -projekt 1 wip -projekt 2 done

--- projekt1/projekt1.c
+++ projekt1/projekt1.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+/*
+ Policz wartości funkcji y = f(x) we wszystkich punktach podziału na n części
+
+przedziału [a,b]. Funkcja f dana jest w postaci rozwinięcia w szereg
+
+potęgowy i w postaci wzoru analitycznego. Obliczanie sumy szeregu
+
+wykonaj z dokładnością ε. Algorytm obliczania sumy szeregu zapisz w
+
+oddzielnej funkcji. Przykładowa funkcja: y=sin(x). Uzupełnij funkcję
+
+obliczającą sumę szeregu tak, by sumowanych było co najwyżej M
+
+wyrazów szeregu. Oznacza to, że przerwanie sumowania może nastąpić
+
+również wtedy, gdy nie zostanie osiągnięta ¡dana dokładność. Informacja
+
+o tym, czy została osiągnięta dokładność czy też nie winna być znana w
+
+funkcji main. Uzupełnij funkcję obliczającą sumę szeregu tak, by w funkcji
+
+main znana była dodatkowo liczba sumowanych wyrazów szeregu.
+ */
+
 int main() {
-    printf("Hello, World!\n");
+    double a, b, epsilon;
+    int p;
+
+    printf("Podaj poczatek i koniec przedzialu [a, b] gdzie x > 0\n");
+    scanf("%le%le", &a, &b);
+
+    printf("podaj wartosc epsilon jaka program uzyje\n");
+    scanf("%le", &epsilon);
+
+    printf("podaj ilosc podprzedzialow\n");
+    scanf("%le", &p);
+
+    for (int i = 0; i < p; ++i) {
+
+    }
     return 0;
 }
---
+++ projekt2/projekt2.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int getAmountOfWords(char string[]) {
+    int wordsAmount = 1;
+    for (int i = 0; i < strlen(string); ++i) {
+        if (string[i] == ' ') {
+            wordsAmount++;
+        }
+    }
+    return wordsAmount;
+}
+
+int getMaxLen(int wordsAmount, int indexArr[]) {
+    int maxLen = 0;
+    for (int i = 0; i < wordsAmount; ++i) {
+        int wordLen = indexArr[i + 1] - indexArr[i] - 1;
+        if (wordLen > maxLen) {
+            maxLen = wordLen;
+        }
+    }
+    return maxLen;
+}
+
+int getIndexOfWord(char string[], char input[]) {
+    int wordsAmount = getAmountOfWords(string);
+    printf("Amount of words: %i\n", wordsAmount);
+    int counter = 1;
+
+    int indexArr[wordsAmount];
+    indexArr[0] = 0;
+
+    for (int i = 0; i < strlen(string); ++i) {
+        if (string[i] == ' ') {
+            indexArr[counter] = i + 1;
+            counter++;
+        }
+    }
+    indexArr[counter] = strlen(string);
+
+    char words[wordsAmount][getMaxLen(wordsAmount, indexArr) + 3];
+
+    for (int i = 0; i < wordsAmount; ++i) {
+        int wordLen = indexArr[i + 1] - indexArr[i] - 1;
+        char word[wordLen];
+
+        for (int j = 0; j <= wordLen + 1; ++j) {
+            word[j] = string[indexArr[i] + j];
+        }
+        word[wordLen] = '\0';
+
+        strcpy(words[i], word);
+    }
+
+    int is = 0, ie = wordsAmount - 1, currentIndex, currentChar, inputChar, lastIndex = 0, timeoutCounter = 0;
+
+    while (1 == 1) {
+        currentIndex = (int) (is + floor((ie - is) / 2));
+
+        if(currentIndex == lastIndex) {
+            timeoutCounter++;
+            if(timeoutCounter == 3) {
+                return -1;
+            }
+        }
+        lastIndex = currentIndex;
+
+//        printf("%s == %s\n", words[currentIndex], input);
+        if (strcmp(words[currentIndex], input) == 0) {
+            return currentIndex + 1;
+        }
+
+        int i = -1;
+        do {
+            i++;
+            inputChar = (int) input[i];
+            currentChar = (int) words[currentIndex][i];
+        } while (inputChar == currentChar);
+
+        if (inputChar > currentChar) {
+            is = currentIndex;
+        } else {
+            ie = currentIndex;
+        }
+    }
+}
+
+int main() {
+    char string[] = "ananas baba banan cytryna drewno ewa framuga gigant kiwi marchew truskawka rozkaz wiatr woda yeti";
+    char input[] = "dr";
+
+    int index = getIndexOfWord(string, input);
+
+    if(index == -1) {
+        printf("Slowa nie znaleziono");
+    } else {
+        printf("indeks slowa to: %i", index);
+    }
+
+
+    return 0;
+}
