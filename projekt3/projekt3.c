f2f343912760529bf7b48e34110a6781982a33f4
jkozub <j.kozub136@gmail.com>
2023-10-16 23:56:01 +0200

    -WIP on matrix alg

--- projekt3/projekt3.c
+++ projekt3/projekt3.c
@@ -1,48 +1,116 @@
 #include <stdio.h>
+#include <math.h>
 
-//https://www.youtube.com/watch?v=YhNrGmgZ9tw
-//https://pl.wikipedia.org/wiki/Metoda_LU
-
-double L[4][4] = {
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1}
+double input[3][3] = {
+        {5, 3, 2},
+        {1, 2, 0},
+        {3, 0, 4}
 };
 
-double U[4][4] = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
+double L[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
 };
 
-double D[4][4] = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
+double U[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
 };
 
-void printData(double data[4][4]) {
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
+void printData(double data[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
             printf("%lf ", data[i][j]);
         }
         printf("\n");
     }
 }
 
-int main() {
-    double data[4][4] = {
-            {1.0, 1.0, 1.0, 1.0},
-            {1.0, 1.0, 1.0, 1.0},
-            {1.0, 1.0, 1.0, 1.0},
-            {1.0, 1.0, 1.0, 1.0}
-    };
-    printData(data);
+void printData2() {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            printf("%lf ", input[i][j]);
+        }
 
-    printf("Test");
+        printf("          ");
+
+        for (int j = 0; j < 3; ++j) {
+            printf("%lf ", L[i][j]);
+        }
+
+        printf("          ");
+
+        for (int j = 0; j < 3; ++j) {
+            printf("%lf ", U[i][j]);
+        }
+        printf("\n");
+    }
+    printf("------------------------------------------------------------------------------------------\n");
+}
+
+int main() {
+    printData2();
+
+    double n;
+    for (int i = 0; i < 3; ++i) {
+        n = 0;
+        for (int j = 1; j < 3; ++j) {
+            n = n + (L[0][j] * U[j][i]);
+        }
+        n = (input[0][i] - n) / L[0][0];
+
+        U[0][i] = n;
+    }
+
+    printData2();
+
+    for (int i = 1; i < 3; ++i) {
+        n = 0;
+        for (int j = 0; j < 3; ++j) {
+            if (j != 0) {
+                n = n + (U[j][0] * L[i][j]);
+            }
+        }
+        n = (input[i][0] - n) / U[0][0];
+
+        L[i][0] = n;
+    }
+
+    printData2();
+
+    for (int i = 1; i < 3; ++i) {
+        n = 0;
+        for (int j = 0; j < 3; ++j) {
+            if (j != 1) {
+                n = n + (L[1][j] * U[j][i]);
+//                printf("%f * %f + ", L[1][j], U[j][i]);
+            }
+        }
+        n = (input[1][i] - n) / L[1][1];
+//        printf("%f", input[1][i]);
+//        printf(" / %f", L[1][1]);
+
+//        printf("== %f\n", n);
+        U[1][i] = n;
+    }
+
+    printData2();
+
+    for (int i = 2; i < 3; ++i) {
+        n = 0;
+        for (int j = 0; j < 3; ++j) {
+            if (j != 1) {
+                n = n + (U[j][1] * L[i][j]);
+            }
+        }
+        n = (input[i][1] - n) / U[1][1];
+
+        L[i][1] = n;
+    }
+
+    printData2();
 
     return 0;
 }
